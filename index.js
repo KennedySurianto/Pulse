9f20e93048bfae675ad7632e83f389ef9b0ea4ca
KennedySurianto <kennedysrnt@gmail.com>
2024-05-28 07:26:13 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+})
