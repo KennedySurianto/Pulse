294894db4ca80859bddfae3ec689fe8323b6e12f
Kennedy Surianto <kennedysrnt@gmail.com>
2024-05-29 08:20:43 +0700

    Update home, login

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import pg from "pg";
+
+dotenv.config();
+
+// const db = new pg.Client({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT
+// })
+// db.connect();
 
 const app = express();
 const port = 3000;
@@ -9,6 +21,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
+})
+
+app.get("/home", (req, res) => {
+    res.render("user_home.ejs");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
 })
 
 app.listen(port, () => {
