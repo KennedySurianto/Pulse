20c2a238c8001ca8dcba233e378061f2937ca82e
KennedySurianto <kennedysrnt@gmail.com>
2024-06-01 17:12:41 +0700

    update: index.js (pg, cookie)

--- index.js
+++ index.js
@@ -4,24 +4,37 @@ import dotenv from "dotenv";
 import pg from "pg";
 import bcrypt from "bcryptjs";
 import passport from "passport";
-import session from "express-session";
+import cookieSession from "cookie-session";
 import { Strategy } from "passport-local";
 
 const app = express();
-const port = 3000;
 const saltRounds = 10;
+const { Pool } = pg;
 
 dotenv.config();
-const db = new pg.Client({
+
+const pool = new Pool({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-});
-db.connect();
+})
 
-app.use(session({
+app.use(async (req, res, next) => {
+    try {
+        req.db = await pool.connect();
+        res.on("finish", () => {
+            req.db.release();
+        })
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+app.use(cookieSession({
+    name: "session",
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
@@ -35,6 +48,9 @@ app.use(passport.session());
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
@@ -75,7 +91,7 @@ app.get("/register", (req, res) => {
 // AUTH
 app.get("/home", ensureAuthenticated, async (req, res) => {
     try {
-        const friends = await db.query("SELECT * FROM friends f JOIN users u ON f.friend_id = u.user_id WHERE f.user_id = $1", [req.user.user_id]);
+        const friends = await req.db.query("SELECT * FROM friends f JOIN users u ON f.friend_id = u.user_id WHERE f.user_id = $1", [req.user.user_id]);
 
         res.render("auth_home.ejs", { message: globalMessage.getMessage(), friends: friends.rows, friendActive: true });
     } catch (error) {
@@ -86,7 +102,7 @@ app.get("/home", ensureAuthenticated, async (req, res) => {
 
 app.get("/pending", ensureAuthenticated, async (req, res) => {
     try {
-        const pending = await db.query("SELECT f.user_id, u.username FROM friends f JOIN users u ON u.user_id = f.user_id WHERE friend_id = $1 EXCEPT SELECT f.friend_id, u.username FROM friends f JOIN users u ON u.user_id = f.friend_id WHERE f.user_id = $1", [req.user.user_id]);
+        const pending = await req.db.query("SELECT f.user_id, u.username FROM friends f JOIN users u ON u.user_id = f.user_id WHERE friend_id = $1 EXCEPT SELECT f.friend_id, u.username FROM friends f JOIN users u ON u.user_id = f.friend_id WHERE f.user_id = $1", [req.user.user_id]);
 
         res.render("auth_pending.ejs", { message: globalMessage.getMessage(), pending: pending.rows, pendingActive: true });
     } catch (error) {
@@ -97,7 +113,7 @@ app.get("/pending", ensureAuthenticated, async (req, res) => {
 
 app.get("/users", ensureAuthenticated, async (req, res) => {
     try {
-        const users = await db.query("SELECT u.user_id, username FROM users u WHERE u.user_id <> $1 AND u.user_id NOT IN (SELECT friend_id FROM friends WHERE user_id = $1)", [req.user.user_id]);
+        const users = await req.db.query("SELECT u.user_id, username FROM users u WHERE u.user_id <> $1 AND u.user_id NOT IN (SELECT friend_id FROM friends WHERE user_id = $1)", [req.user.user_id]);
         res.render("auth_userlist.ejs", { users: users.rows, userActive: true });
     } catch (error) {
         console.log(error);
@@ -117,10 +133,10 @@ app.get("/profile", ensureAuthenticated, async (req, res) => {
 app.get("/chat/:friend_id", ensureAuthenticated, async (req, res) => {
     const friend_id = req.params.friend_id;
     try {
-        const messages = await db.query("SELECT DISTINCT c.*, m.*, u.username FROM chats c JOIN participants p ON p.chat_id = c.chat_id JOIN messages m ON m.chat_id = c.chat_id JOIN users u ON m.sender_id = u.user_id WHERE p.user_id IN ($1, $2) ORDER BY m.created_at ASC", [req.user.user_id, friend_id]);
+        const messages = await req.db.query("SELECT DISTINCT c.*, m.*, u.username FROM chats c JOIN participants p ON p.chat_id = c.chat_id JOIN messages m ON m.chat_id = c.chat_id JOIN users u ON m.sender_id = u.user_id WHERE p.user_id IN ($1, $2) ORDER BY m.created_at ASC", [req.user.user_id, friend_id]);
         console.log(messages.rows);
 
-        const friend = await db.query("SELECT user_id, username FROM users WHERE user_id = $1", [friend_id]);
+        const friend = await req.db.query("SELECT user_id, username FROM users WHERE user_id = $1", [friend_id]);
 
         res.render("auth_chat.ejs", { messages: messages.rows, friend: friend.rows[0] });
     } catch (error) {
@@ -131,9 +147,9 @@ app.get("/chat/:friend_id", ensureAuthenticated, async (req, res) => {
 
 app.post("/remove-friend", ensureAuthenticated, async (req, res) => {
     const friend_id = parseInt(req.body.friend_id);
-    
+
     try {
-        await db.query("DELETE FROM friends WHERE user_id = $1 AND friend_id = $2", [req.user.user_id, friend_id]);
+        await req.db.query("DELETE FROM friends WHERE user_id = $1 AND friend_id = $2", [req.user.user_id, friend_id]);
 
         res.redirect("/home");
     } catch (error) {
@@ -146,24 +162,24 @@ app.post("/add-friend", ensureAuthenticated, async (req, res) => {
     const friend_id = parseInt(req.body.user_id);
     // console.log(friend_id);
     try {
-        await db.query("BEGIN;");
-        await db.query("INSERT INTO friends (user_id, friend_id) VALUES ($1, $2)", [
+        await req.db.query("BEGIN;");
+        await req.db.query("INSERT INTO friends (user_id, friend_id) VALUES ($1, $2)", [
             req.user.user_id,
             friend_id
         ]);
-        const chat = await db.query("INSERT INTO chats (chat_id) VALUES (DEFAULT) RETURNING *");
-        await db.query("INSERT INTO participants (user_id, chat_id) VALUES ($1, $3), ($2, $3)", [
+        const chat = await req.db.query("INSERT INTO chats (chat_id) VALUES (DEFAULT) RETURNING *");
+        await req.db.query("INSERT INTO participants (user_id, chat_id) VALUES ($1, $3), ($2, $3)", [
             req.user.user_id,
             friend_id,
             chat.rows[0].chat_id
         ]);
-        await db.query("COMMIT");
+        await req.db.query("COMMIT");
 
         globalMessage.setMessage("success", "Friend added successfully", "Try chatting now");
 
         res.redirect("/home");
     } catch (error) {
-        await db.query("ROLLBACK");
+        await req.db.query("ROLLBACK");
 
         console.log(error);
         res.redirect("/");
@@ -177,7 +193,7 @@ app.post("/message-post", async (req, res) => {
     const friend_id = parseInt(req.body.friend_id);
 
     try {
-        await db.query("INSERT INTO messages (sender_id, content, chat_id) VALUES ($1, $2, $3)", [sender_id, content, chat_id]);
+        await req.db.query("INSERT INTO messages (sender_id, content, chat_id) VALUES ($1, $2, $3)", [sender_id, content, chat_id]);
 
         res.redirect(`/chat/${friend_id}`);
     } catch (error) {
@@ -205,7 +221,7 @@ app.post("/register-post", async (req, res) => {
     const password_confirmation = req.body.password_confirmation;
 
     try {
-        const checkUnique = await db.query("SELECT * FROM users WHERE username = $1", [username]);
+        const checkUnique = await req.db.query("SELECT * FROM users WHERE username = $1", [username]);
 
         if (checkUnique.rowCount > 0) {
             globalMessage.setMessage("danger", "Username already exist", "Try using a different username");
@@ -219,7 +235,7 @@ app.post("/register-post", async (req, res) => {
                 if (err) {
                     console.log(err);
                 } else {
-                    const result = await db.query("INSERT INTO users (username, password_hash) VALUES ($1, $2) RETURNING *", [username, hash]);
+                    const result = await req.db.query("INSERT INTO users (username, password_hash) VALUES ($1, $2) RETURNING *", [username, hash]);
                     const user = result.rows[0];
 
                     req.login(user, (err) => {
@@ -243,9 +259,10 @@ app.post("/login-post", passport.authenticate("local", {
 passport.use(
     "local",
     new Strategy(
-        async (username, password, done) => {
+        { passReqToCallback: true },
+        async (req, username, password, done) => {
             try {
-                const result = await db.query("SELECT * FROM users WHERE username = $1", [username]);
+                const result = await req.db.query("SELECT * FROM users WHERE username = $1", [username]);
                 if (result.rowCount === 0) {
                     globalMessage.setMessage("danger", "Username doesn't exist", "Please make sure you entered the correct username");
                     return done(null, false);
@@ -280,6 +297,6 @@ passport.deserializeUser((user, cb) => {
     cb(null, user);
 })
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}.`);
+app.listen(process.env.PORT, '0.0.0.0', () => {
+    console.log(`Listening on port ${process.env.PORT}.`);
 })
