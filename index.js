575339a1efc1b75c544c999ba6efadb6dbc660d7
KennedySurianto <kennedysrnt@gmail.com>
2024-06-02 21:48:43 +0700

    fix: index.js

--- index.js
+++ index.js
@@ -21,18 +21,18 @@ const { Pool } = pg;
 
 dotenv.config();
 // Development
-// const pool = new Pool({
-//     user: process.env.PG_USER,
-//     host: process.env.PG_HOST,
-//     database: process.env.PG_DATABASE,
-//     password: process.env.PG_PASSWORD,
-//     port: process.env.PG_PORT,
-// })
+const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+})
 
 // Production
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
-})
+// const pool = new Pool({
+//     connectionString: process.env.POSTGRES_URL,
+// })
 
 app.use(async (req, res, next) => {
     try {
@@ -59,7 +59,7 @@ app.use(cookieSession({
 // Set up storage engine
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/images/'); // Destination folder for uploaded files
+        cb(null, './public/images/'); // Destination folder for uploaded files
     },
     filename: (req, file, cb) => {
         // Generate unique filename
